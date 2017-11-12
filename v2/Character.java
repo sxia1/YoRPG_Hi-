f5b69a15651c888b413a8c131a2435dce4935e2e
unknown <sxia1@stuy.edu>
2017-11-12 17:15:15 -0500

    changes

---
+++ v2/Character.java
@@ -0,0 +1,44 @@
+//Team Hi?
+//Roster: Daniel Jiang, Woosuk Lee, Sophia Xia
+//APCS1 pd1
+//HW30 -- Ye Olde Role Playing Game, Improved
+//2017-11-12
+
+public class Character{
+
+    //Attributes
+    //----------------------------------
+    public int health;
+    public int strength;
+    public int defense;
+    public double attRating;
+
+    //Methods
+    //----------------------------------
+    //isAlive
+    public Boolean isAlive() {
+	//Checks to see if the chara is still alive
+	return (health > 0);
+    }
+
+    //getDefense
+    public int getDefense() {
+	//retrieves the defense stat so it may be used in battle calculations
+	return defense;
+    }
+
+    //lowerHP
+    public void lowerHP( int num ) {
+	//Used to lower the char's health if they be damaged
+	health = health - num;
+    }
+
+    //attack
+    public int attack( Character chara ) {
+	int damage = (int)(strength * attRating - chara.getDefense());
+	chara.lowerHP(damage);
+	return damage;
+	// calculates how much dmg the chara will do to the other chara taking into consideration the chara's overall attack power and the otherchara's defenses
+    }
+
+}//end Character
