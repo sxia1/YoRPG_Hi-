de4d299d9fe8ec3659dfeeb748b04cd2e42936e9
unknown <sxia1@stuy.edu>
2017-11-13 19:21:03 -0500

    changes

--- v3/Character.java
+++ v3/Character.java
@@ -8,10 +8,10 @@ public class Character{
 
     //Attributes
     //----------------------------------
-    public int health;
-    public int strength;
-    public int defense;
-    public double attRating;
+    protected int health;
+    protected int strength;
+    protected int defense;
+    protected double attRating;
 
     //Methods
     //----------------------------------
@@ -22,20 +22,23 @@ public class Character{
     }
 
     //getDefense
-    public int getDefense() {
+    protected int getDefense() {
 	//retrieves the defense stat so it may be used in battle calculations
 	return defense;
     }
 
     //lowerHP
-    public void lowerHP( int num ) {
+    protected void lowerHP( int num ) {
 	//Used to lower the char's health if they be damaged
 	health = health - num;
     }
 
     //attack
-    public int attack( Character chara ) {
+    protected int attack( Character chara ) {
 	int damage = (int)(strength * attRating - chara.getDefense());
+	if (damage < 0){
+	    damage = 0;
+	}
 	chara.lowerHP(damage);
 	return damage;
 	// calculates how much dmg the chara will do to the other chara taking into consideration the character's overall attack power 
--- v3/Mage.java
+++ v3/Mage.java
@@ -1,9 +1,12 @@
 public class Mage extends Protagonist{
 
     //attributes
+    //---------------------------------------
     private int mana;
 
+    
     //Constructor
+    //---------------------------------------
     public Mage(String nombre){
 	super(nombre);
 	mana = 50;
@@ -14,6 +17,14 @@ public class Mage extends Protagonist{
 
     //methods
     //---------------------------------------
+
+    //ABOUT
+    //description of the class
+    public static String about(){
+	return "Mage: Uses mana to increase attack power while keeping defenses the same. However, mana does run out so use it wisely. A mage has lower strength, but a higher attack rating.";
+    }//end about
+
+    
     //SPECIALIZE
     //expends mana in return for more offensive power, defense is not changed
     public void specialize(){
@@ -25,6 +36,7 @@ public class Mage extends Protagonist{
 	    System.out.println("Ye hath no more mana");
 	}
     }//end specialize
+
     
     //normalize
     //returns stats back to normal
