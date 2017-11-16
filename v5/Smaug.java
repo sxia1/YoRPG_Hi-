8c524e098c95c4cd1a6c451ef14bfde4f731e3dd
Daniel Jiang <danieljiang@Daniels-MacBook-Air.local>
2017-11-16 00:06:13 -0500

    Final Boss added

---
+++ v5/Smaug.java
@@ -0,0 +1,20 @@
+public class Smaug extends Monster {
+
+    private String name = "Smaug";
+       
+    public Smaug(){
+	super();
+	health = 300;
+	strength = 50;
+	defense = 30;
+	attRating = 1.5;
+    }
+
+    public status String about(){
+	return "Come now, don't be shy... step into the light";
+    }
+
+    public String getName(){
+	return name;
+    }
+}
